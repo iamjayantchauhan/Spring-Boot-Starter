ae3de333624b7abcdd7a2e713f8ca345b8884d3d
Jayant Chauhan <jayantjagdishchauhan@gmail.com>
2020-12-29 07:26:07 +0530

    Add user APIs

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -4,10 +4,9 @@ import com.example.demo.models.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -23,5 +22,15 @@ public class UserController {
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<User> createUser(@RequestBody User user) {
         return userService.createUser(user);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<User>> getAllUsers() {
+        return userService.getAllUserList();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{userId}")
+    public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
+        return userService.getSingleUser(userId);
     }
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -4,8 +4,12 @@ import com.example.demo.models.User;
 import com.example.demo.repositories.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -21,5 +25,22 @@ public class UserService {
         Object dbObject = userRepository.save(user);
         User user1 = new ObjectMapper().convertValue(dbObject, User.class);
         return ResponseEntity.ok(user1);
+    }
+
+    public ResponseEntity<List<User>> getAllUserList() {
+        return ResponseEntity.ok(userRepository.findAll());
+    }
+
+    public ResponseEntity<User> getSingleUser(String userId) {
+        try {
+            Optional<User> getUser = userRepository.findById(userId);
+            if (getUser.isPresent()) {
+                return ResponseEntity.status(HttpStatus.OK).body(getUser.get());
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
 }
